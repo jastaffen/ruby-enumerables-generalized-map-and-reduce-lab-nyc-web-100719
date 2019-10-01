a72c7a89b129df6a1e89023281add3b1363932f5
Jack Staffen <jack.staffen@gmail.com>
2019-10-01 02:15:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,6 @@
 # Your Code Here
-def map_to_negativize(array)
-  new_arr = array.map { |num| num * -1 }
-  new_arr
+def map(array)
+  yield(array)
 end
+
+map([1,2,3]) { |num| new_arr = []; new_arr << num * -1 }
